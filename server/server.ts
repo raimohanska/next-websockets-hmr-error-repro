ef7d483241b251d066cd42ef5f2bcffd1c5f6627
Juha Paananen <juha.paananen@gmail.com>
2022-11-01 13:04:54 +0200

    Introduce solution based on replacing express-ws with plain ws.

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import next from "next";
 import * as Http from "http";
+import * as WS from "ws";
 import expressWs from "express-ws";
+import { parse } from "url";
 
 const port = parseInt(process.env.PORT || "3000");
 const dev = process.env.NODE_ENV !== "production";
@@ -11,8 +13,13 @@ const nextApp = next({ dev });
 const nextJsHandle = nextApp.getRequestHandler();
 
 nextApp.prepare().then(() => {
-  // Comment out the following to fix HMR
-  setupWebsockets();
+  /* 
+    Using express-ws causes the Next.js HMR issue to manifest.
+    If you switch to plain `ws`, the application works just fine.
+
+  */
+  setupWebsocketsUsingExpressWs();
+  //setupWebsocketsUsingWs();
 
   app.all("*", (req, res) => {
     console.log("Request to Next.js", req.url);
@@ -22,10 +29,26 @@ nextApp.prepare().then(() => {
   http.listen(3000, () => console.log(`Listening at http://localhost:${port}`));
 });
 
-function setupWebsockets() {
+function setupWebsocketsUsingExpressWs() {
   const ws = expressWs(app, http, { leaveRouterUntouched: true });
   ws.app.ws("/myspecialsocket", (ws, req) => {
     console.log("Socket connected");
     ws.send("Welcome to the socket");
+  });
+}
+
+function setupWebsocketsUsingWs() {
+  const wss = new WS.WebSocketServer({
+    noServer: true,
+  });
+
+  http.on("upgrade", (request, socket, head) => {
+    const { pathname } = parse(request.url!);
+    if (pathname === "/myspecialsocket") {
+      wss.handleUpgrade(request, socket, head, function done(ws) {
+        console.log("Socket connected");
+        ws.send("Welcome to the socket");
+      });
+    }
   });
 }
